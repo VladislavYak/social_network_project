a356316ac8d4f840e0eb84f58d6e52930c59181b
vladiyakovlev <vladiyakovlev@ozon.ru>
2022-05-24 00:31:29 +0300

    some html subblocks added

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('foobar')
+    template = 'posts/index.html'
+    return render(request, template)
 
 def group_posts(request, slug):
     return HttpResponse(f'barfoo + {slug}')
